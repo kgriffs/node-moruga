b886daee4b1866051560e4e342aca5aacce77c7f
kurt-griffiths <kurt.griffiths@rackspace.com>
2012-07-03 15:37:26 -0400

    Added regex filters example

--- filters.example.js
+++ filters.example.js
@@ -3,10 +3,19 @@ exports.pre = [
   {
     name: 'Hunt for bacon',
     path: '/chunky-bacon',
-    action: function(req, resp) {
+    action: function(req, res) {
       // End the response stream to tell moruga to halt processing
       // and bail out of this request without proxying it.
-      resp.end("Soooooo chunky.");
+      res.end('Soooooo chunky.');
+    }
+  },
+  {
+    name: 'Check for breakfast',
+    path: new RegExp('/(bacon|eggs|ham|sausage|pancakes|toast|juice|milk|coffee|spam|/)+$', 'i'),
+    action: function(req, res) {
+      // End the response stream to tell moruga to halt processing
+      // and bail out of this request without proxying it.
+      res.end("Let's eat!");
     }
   }
 ]
